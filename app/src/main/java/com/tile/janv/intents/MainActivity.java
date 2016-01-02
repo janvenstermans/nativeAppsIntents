25c31d6b436ae22f74b1c706072e41dcaa8f1b47
janv <janvenstermans@gmail.com>
2016-01-02 19:03:48 +0100

    improvements

--- app/src/main/java/com/tile/janv/intents/MainActivity.java
+++ app/src/main/java/com/tile/janv/intents/MainActivity.java
@@ -1,34 +1,59 @@
 package com.tile.janv.intents;
 
+import android.app.ListFragment;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SPEACH_ID = 2;
 
-    private TextView speechResult;
+    //        http://androidexample.com/Create_A_Simple_Listview_-_Android_Example/index.php?view=article_discription&aid=65&aaid=90
+//        see example from http://developer.android.com/training/basics/fragments/fragment-ui.html
+    protected ListFragment speechResult;
+
+    private int speechResultLayout;
+
+    //---------------------
+    // lifecycle methods
+    //---------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        speechResult = (TextView) findViewById(R.id.text_speech_result);
+        ButterKnife.bind(this);
+        speechResult = (ListFragment) getFragmentManager().findFragmentById(R.id.text_speech_result);
+        speechResult.getListView().setChoiceMode(ListView.CHOICE_MODE_NONE);
+        speechResult.setListAdapter(new ArrayAdapter<String>(this, speechResultLayout,
+                new ArrayList<String>()));
+
+        // We need to use a different list item layout for devices older than Honeycomb
+        speechResultLayout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
     }
+
+    //---------------------
+    // menu methods
+    //---------------------
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //---------------------
+    // speak intent methods
+    //---------------------
+
+    @OnClick(R.id.speak_button)
     public void onSpeakButtonClicked(View view) {
         // good help: http://viralpatel.net/blogs/android-speech-to-text-api/
         Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -62,10 +92,9 @@ public class MainActivity extends AppCompatActivity {
         PackageManager packageManager = getPackageManager();
         ComponentName cn = speechIntent.resolveActivity(packageManager);
         if (cn == null) {
-            Toast t = Toast.makeText(getApplicationContext(),
-                    "Opps! Your device doesn't support Speech to Text",
-                    Toast.LENGTH_SHORT);
-            t.show();
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speak_app_not_available),
+                    Toast.LENGTH_SHORT).show();
         } else {
             startActivityForResult(speechIntent, SPEACH_ID);
         }
@@ -78,21 +107,26 @@ public class MainActivity extends AppCompatActivity {
             switch (resultCode) {
                 case RESULT_OK:
                     if (null != data) {
-                        ArrayList<String> text = data
+                        ArrayList<String> textList = data
                                 .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
                         // array is list of possible interpretations. Just show first (best).
-                        speechResult.setText(text.get(0));
+                        speechResult.setListAdapter(new ArrayAdapter<String>(this, speechResultLayout,
+                                textList));
                     }
                     break;
                 case RESULT_CANCELED:
                     Toast.makeText(getApplicationContext(),
-                            "Speech to text canceled",
+                            getString(R.string.speech_to_text_cancelled),
                             Toast.LENGTH_SHORT).show();
                     break;
             }
         }
     }
+
+    //---------------------
+    // private methods
+    //---------------------
 
     private void findPackageInGooglePlay(String packageName) {
         // If there is no Activity available to perform the action
